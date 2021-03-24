dc38ac426e886080fa6fbeacced310fa5a49a9a3
Richard Crippen <richard.crippen@gmail.com>
2021-03-24 11:22:23 -0400

    small style changes, adding classes and IDs in HTML, starting JS code

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+//Declaration of variables for input
+var userFormEl = document.getElementById("user-form")
+var userCityNameEl = document.getElementById("city-name")
+var searchBtnEl = document.getElementById("searchBtn")
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("searching for city")
+});
