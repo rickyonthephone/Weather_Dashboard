8474a9fb06dbfe95ff9a08885071c41be6cb33bd
Richard Crippen <richard.crippen@gmail.com>
2021-03-24 20:34:32 -0400

    minor CSS and HTML updates, got API call working on JS code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,42 @@
-//Declaration of variables for input
+//Declaration of variables for form input
 var userFormEl = document.getElementById("user-form")
 var userCityNameEl = document.getElementById("city-name")
 var searchBtnEl = document.getElementById("searchBtn")
+var citySelect = userCityNameEl.value.trim();
 
-searchBtn.addEventListener("click", function(event) {
-    event.preventDefault();
-    console.log("searching for city")
-});
+//Prevent default on seach button
+function searchCity (searchEvent) {
+    searchEvent.preventDefault();
+    //Get city name from form input
+    var citySelect = userCityNameEl.value.trim();
+    
+    // console.log("The city you selected is " + citySelect);
+
+    if(citySelect) {
+        getUserWeather(citySelect);
+        console.log("I have city " + citySelect);
+    }else {
+        alert('City name is required to search.')
+    }
+
+}
+
+function getUserWeather(name) {
+    console.log('Searching for city ' + name);
+
+//Take above parameter to build URL string
+
+var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q='+name+'&appid=0642f62247cd5fc4b4b2603fcde8ec95';
+//API request 
+
+fetch(apiURL)
+    .then(response =>{
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+    })
+    
+}
+
+searchBtn.addEventListener("click", searchCity);
